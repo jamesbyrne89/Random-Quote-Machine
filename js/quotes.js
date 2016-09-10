5c7ef1163c3dc808a560d7ac66a3548b8011c0e2
jamesbyrne89 <mail@jamestbyrne.com>
2016-09-10 17:46:18 +0100

    Full CSS with media queries. No social icons

--- js/quotes.js
+++ js/quotes.js
@@ -5,8 +5,8 @@ $('#generate').on('click', function(e) {
       url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-source').text(post.title);
-        $('#quote-holder').html(post.content);
+        $('#quote-source').text('-   ' + post.title).animate({opacity:1},400);
+        $('#quote-holder').html(post.content).animate({opacity:1},400);
       },
       cache: false
     });
