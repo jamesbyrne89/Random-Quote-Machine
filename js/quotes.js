913e3b4f5d97efc1a05b39c94f4bcf5e25b835c2
jamesbyrne89 <mail@jamestbyrne.com>
2016-09-10 20:21:14 +0100

    changed API url to https

--- js/quotes.js
+++ js/quotes.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 $('#generate').on('click', function(e) {
     e.preventDefault();
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-source').text('-   ' + post.title).animate({opacity:1},400);
+        $('#quote-source').text('~   ' + post.title).animate({opacity:1},400);
         $('#quote-holder').html(post.content).animate({opacity:1},400);
       },
       cache: false
