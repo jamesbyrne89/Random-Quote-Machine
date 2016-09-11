ec703338cfb75a373f6aab2e31d307fe1f801b71
jamesbyrne89 <mail@jamestbyrne.com>
2016-09-11 01:55:00 +0100

    Added tweet button functionality and styling

--- js/quotes.js
+++ js/quotes.js
@@ -5,20 +5,18 @@ $('#generate').on('click', function(e) {
       url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-source').text('~   ' + post.title).animate({opacity:1},400);
+        var quoteSource = (post.title);
+        var currentQuote = (post.content);
+        $('#quote-source').html('~   ' + post.title).animate({opacity:1},400);
         $('#quote-holder').html(post.content).animate({opacity:1},400);
+        $('#tweet-button').animate({opacity:1},400);
+        var noTags = currentQuote.replace(/(<([^>]+)>)/gi,"");
+          $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + noTags + "- " + quoteSource));
       },
       cache: false
     });
   });
-
-
-$('#generate').on('click', function() {
-
-$('.mobile-button').html("Scroll down");
-
 });
+  
 
 
-
-});
