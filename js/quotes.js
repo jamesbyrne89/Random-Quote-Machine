1c743217479377520581a25f3719dc8248b03ee9
jamesbyrne89 <mail@jamestbyrne.com>
2016-12-01 10:37:49 +0000

    Added site title, Apple icon and favicon

--- js/quotes.js
+++ js/quotes.js
@@ -8,7 +8,7 @@ $('#generate').on('click', function(e) {
         var quoteSource = (post.title);
         var currentQuote = (post.content);
         $('#quote-source').html('~   ' + post.title).animate({opacity:1},400);
-        $('#quote-holder').html(post.content).animate({opacity:1},400);
+        $('#quote-holder').html(post.content).delay(2000).animate({opacity:1},400);
         $('#tweet-button').delay(2000).animate({opacity:1},600);
         var noTags = currentQuote.replace(/(<([^>]+)>)/gi,"'");
           $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?&related=freecodecamp&text=' + encodeURIComponent(noTags +' - ' + quoteSource));
