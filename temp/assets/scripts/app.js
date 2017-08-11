aad826eed3e783fadaf09c84e4fc6220a175caa2
jamesbyrne89 <mail@jamestbyrne.com>
2017-08-11 21:45:05 +0100

    Reorganised file structure

---
+++ temp/assets/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+$('#generate').on('click', function () {
+  $("#quote-holder").addClass("reset");
+  $("#quote-holder").removeClass("executed");
+  $("#quote-source").delay(1000).addClass("reset");
+  $("#quote-source").delay(1000).removeClass("executed");
+  $("#tweet-button").addClass("reset");
+  $("#tweet-button").removeClass("executed");
+  $("#tweet-quote").delay(10000).addClass("reset");
+  $("#tweet-quote").delay(10000).removeClass("executed");
+  setTimeout(function () {
+    $.ajax({
+      crossOrigin: true,
+      url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback",
+      dataType: "jsonp"
+    });
+  }, 300);
+});
+function mycallback(json) {
+  var quote = json[0];
+  $("#quote-holder").html(quote.content);
+  $("#quote-source").html("~ " + quote.title);
+  $("#quote-holder").addClass("executed");
+  $("#quote-holder").removeClass("reset");
+  $("#quote-source").delay(1000).addClass("executed");
+  $("#quote-source").delay(1000).removeClass("reset");
+  $("#tweet-button").addClass("executed");
+  $("#tweet-button").removeClass("reset");
+  $("#tweet-quote").delay(10000).addClass("executed");
+  $("#tweet-quote").delay(10000).removeClass("reset");
+  var noTags = quote.content.replace(/(<([^>]+)>)/gi, "'");
+  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?&text=' + encodeURIComponent(noTags + ' - ' + quote.title));
+}
