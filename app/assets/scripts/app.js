6d4bced1fafb363f75330bdd3b5c55be4b57e087
jamesbyrne89 <mail@jamestbyrne.com>
2017-08-12 16:31:15 +0100

    Completed desktop version

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,34 +1,44 @@
-$('#generate').on('click', function() {
-        $("#quote-holder").addClass("reset");
-        $("#quote-holder").removeClass("executed");
-        $("#quote-source").delay(1000).addClass("reset");
-        $("#quote-source").delay(1000).removeClass("executed");
-        $("#tweet-button").addClass("reset");
-        $("#tweet-button").removeClass("executed");
-                $("#tweet-quote").delay(10000).addClass("reset");
-        $("#tweet-quote").delay(10000).removeClass("executed");
-        setTimeout(function(){ 
-          $.ajax({
-            crossOrigin: true,
-            url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback",
-            dataType:"jsonp"
-        });
-        }, 300); 
-  });
-  function mycallback(json){
-    var quote = json[0];
-    $("#quote-holder").html(quote.content);
-    $("#quote-source").html("~ " + quote.title);
-    $("#quote-holder").addClass("executed");
-    $("#quote-holder").removeClass("reset");
-    $("#quote-source").delay(1000).addClass("executed");
-    $("#quote-source").delay(1000).removeClass("reset");
-    $("#tweet-button").addClass("executed");
-    $("#tweet-button").removeClass("reset");
-        $("#tweet-quote").delay(10000).addClass("executed");
-    $("#tweet-quote").delay(10000).removeClass("reset");
-    var noTags = (quote.content).replace(/(<([^>]+)>)/gi,"'");
-          $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?&text=' + encodeURIComponent(noTags +' - ' + (quote.title)));
-  }
+  const getQuoteBtn = document.getElementById('new-quote-btn');
+  const quoteContainer = document.getElementById('quote-holder');
+  const quoteBody = document.getElementById('quote-text');
+  const quoteSource = document.getElementById('quote-source');
+  const btnWrapper = document.getElementById('btn-wrapper');
 
+/*
+Get a new quote from the API
+ */
+  function getAQuote() {
+      $.ajax({
+          url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+          success: function(data) {
 
+              let post = data.shift(); // The data is an array of posts. Grab the first one. 
+
+              btnWrapper.innerHTML = `<a class="button tweet-button" id="tweet-quote" title="Tweet this quote!" target="_blank" href="https://twitter.com/intent/tweet?text=${quoteText} ~ ${post.title}"
+  data-size="large">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                    <a class='button fb-button' target='_blank' OnClick="window.open(this.href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false;" href='http://www.facebook.com/sharer/sharer.php?u=https://jamesbyrne89.github.io/Random-Quote-Machine&title=Design Wisdom'>
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    </a>`;
+
+              quoteBody.innerHTML = `<div id='quote-mark' class='quote__mark'>​‌“</div>${post.content}`;
+              quoteSource.innerHTML = `<span>${post.title}</span>`;
+
+              let quoteText = document.getElementsByTagName('p')[0].innerHTML;
+              document.getElementById('tweet-quote').setAttribute('href', `https://twitter.com/intent/tweet?text=${quoteText}%20~%20${post.title}`);
+          },
+          cache: false
+      });
+
+  };
+
+  getQuoteBtn.addEventListener('click', getAQuote);
+
+  document.body.addEventListener('keyup', function(e) {
+      if (e.keyCode === 32 || e.keyCode === 13) {
+          getAQuote();
+      }
+  })
+
+  getAQuote();
